cbdf6a14cf3c504fa0e95363091fc79bbb387b3d
s1270193 <66015492+s1270193@users.noreply.github.com>
2020-08-06 19:46:07 +0900

    Add files via upload

---
+++ HeadorTail.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(){
+    char name[40];
+    int i,heads=0,tails=0;
+    
+    srand((unsigned int)time(NULL));
+    
+    printf("Who are you? \n>");
+    scanf("%s", name);
+    printf("Hello, %s! \n", name);
+
+    printf("Tossing a coin...\n");
+
+    for (i = 1; i < 4; i++)
+    {
+        if(0 == rand() % 2)
+        {
+            heads++;
+            printf("Round %d: Heads\n", i );
+        }
+        else
+        {
+            tails++;
+            printf("Round %d: Tails\n", i );
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    if(heads > tails)
+    {
+        printf("%s won!\n", name);
+    }
+    
+    else
+    {
+        printf("%s lost!\n", name);
+    }
+
+    return 0;
+}
